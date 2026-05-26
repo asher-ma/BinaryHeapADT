17f8344bf099220195a53aa1517ac497c7b3334f
asher-ma <mangelas@uw.edu>
2026-05-26 17:05:21 +0000

    Bug fixes

--- Include/HeapSort.h
+++ Include/HeapSort.h
@@ -1,7 +1,7 @@
 #ifndef _HEAP_SORT_H_
 #define _HEAP_SORT_H_
 
-template <typeName ItemType>
+template <typename ItemType>
 void heapSort(ItemType someArray[], const int arraySize);
 
 #include "../src/HeapSort.cpp"
