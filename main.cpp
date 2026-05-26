7f64401d6785a10fd08f4a3721831c4afcd65429
asher-ma <mangelas@uw.edu>
2026-05-26 06:53:03 +0000

    Implements ArrayMaxHeap tests

--- main.cpp
+++ main.cpp
@@ -1,3 +1,31 @@
-int main () {
+#include <iostream>
+#include <vector>
+
+#include "ArrayMaxHeap.h"
+
+int main (){
+    // Make array
+    int array[7] = {2, 5, 8, 1, 3, 6, 10};
+    std::cout << "Array: ";
+    for (int x : array) {
+        std::cout << x << " ";
+    }
+
+    // Heap it
+    std::cout << "\nBuilding heap...\n";
+    ArrayMaxHeap<int> heap{array, 7};
+
+    // Popping
+    std::cout << "\nDeconstructing heap:\n";
+    while (!heap.isEmpty()) {
+        std::cout << heap.peekTop() << " ";
+        heap.remove();
+    }
+    std::cout << "\n";
+
+    // Clear
+    std::cout << "\nClearing Heap...\n";
+    heap.clear();
+    std::cout << "isEmpty: " << (heap.isEmpty() ? "true" : "false") << "\n";
     return 0;
 }
