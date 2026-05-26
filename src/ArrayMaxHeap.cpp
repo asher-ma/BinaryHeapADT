2fc9985951c0413fa62b3555ca7334d703ee152d
asher-ma <asherhmangel@gmail.com>
2026-05-26 02:22:46 +0000

    Adds function headers

---
+++ src/ArrayMaxHeap.cpp
@@ -0,0 +1,59 @@
+#include "../Include/HeapInterface.h"
+#include "../Include/ArrayMaxHeap.h"
+
+
+// Constructors ------------------------------------------------------------------------------------------
+
+template <typename ItemType>
+ArrayMaxHeap<ItemType>::ArrayMaxHeap() {}
+
+template <typename ItemType>
+ArrayMaxHeap<ItemType>::ArrayMaxHeap(const ItemType someArray[], const int arraySize) {}
+
+template <typename ItemType>
+ArrayMaxHeap<ItemType>::~ArrayMaxHeap() {}
+
+
+// Public methods -----------------------------------------------------------------------------------------
+
+template <typename ItemType>
+bool ArrayMaxHeap<ItemType>::isEmpty() const {}
+
+template <typename ItemType>
+int ArrayMaxHeap<ItemType>::getNumberOfNodes() const {}
+
+template <typename ItemType>
+int ArrayMaxHeap<ItemType>::getHeight() const {}
+
+template <typename ItemType>
+ItemType ArrayMaxHeap<ItemType>::peekTop() const {}
+
+template <typename ItemType>
+bool ArrayMaxHeap<ItemType>::add(const ItemType& newData) {}
+
+template <typename ItemType>
+bool ArrayMaxHeap<ItemType>::remove() {}
+
+template <typename ItemType>
+void ArrayMaxHeap<ItemType>::clear() {}
+
+
+// Private methods ----------------------------------------------------------------------------------------
+
+template <typename ItemType>
+int ArrayMaxHeap<ItemType>::getLeftChildIndex(const int nodeIndex) const {}
+
+template <typename ItemType>
+int ArrayMaxHeap<ItemType>::getRightChildIndex(int nodeIndex) const {}
+
+template <typename ItemType>
+int ArrayMaxHeap<ItemType>::getParentIndex(int nodeIndex) const {}
+
+template <typename ItemType>
+bool ArrayMaxHeap<ItemType>::isLeaf(int nodeIndex) const {}
+
+template <typename ItemType>
+void ArrayMaxHeap<ItemType>::heapRebuild(int subTreeRootIndex) {}
+
+template <typename ItemType>
+void ArrayMaxHeap<ItemType>::heapCreate() {}
