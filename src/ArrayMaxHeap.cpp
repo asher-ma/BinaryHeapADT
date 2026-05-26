c67556ae0d6c05c96b3cf5d6db9675f455ba3974
asher-ma <asherhmangel@gmail.com>
2026-05-26 03:07:09 +0000

    Implements heapRebuild()

--- src/ArrayMaxHeap.cpp
+++ src/ArrayMaxHeap.cpp
@@ -14,9 +14,11 @@ ArrayMaxHeap<ItemType>::ArrayMaxHeap()
 template <typename ItemType>
 ArrayMaxHeap<ItemType>::ArrayMaxHeap(const ItemType someArray[], const int arraySize)
         : itemCount(arraySize), maxItems(DEFAULT_CAPACITY), items(new ItemType[maxItems]) {
+    // Copy array contents into heap array
     for (int i = 0; i < arraySize; i++) {
         this->items[i] = someArray[i];
     }
+    // Reorder contents to obey heap condition
     this->heapCreate();
 }
 
@@ -88,8 +90,35 @@ bool ArrayMaxHeap<ItemType>::isLeaf(int nodeIndex) const {
     return false;
 }
 
+// If a child is larger than the parent, swap the largest child with parent
+// If swap occurs, recursively call heapRebuild on orginal node, moving it down
+// heap until it is larger than both children
 template <typename ItemType>
-void ArrayMaxHeap<ItemType>::heapRebuild(int subTreeRootIndex) {}
+void ArrayMaxHeap<ItemType>::heapRebuild(int subTreeRootIndex) {
+    int l = getLeftChildIndex(subTreeRootIndex);
+    int r = getRightChildIndex(subTreeRootIndex);
+    int largest = subTreeRootIndex;
+
+    // If left child is larger than parent
+    if (l <= itemCount && items[l] > items[subTreeRootIndex]) {
+        // Overwrite largest with left child
+        largest = l;
+    }
+    // If right child is larger than left child
+    if (r <= itemCount && items[r] > items[largest]) {
+        // Overwrite largest with right child
+        largest = r;
+    }
+    // If largest is not parent
+    if (largest != subTreeRootIndex) {
+        // Swap parent with largest
+        int temp = items[subTreeRootIndex]; // TODO: Use ItemType instead of int(?)
+        items[subTreeRootIndex] = items[largest];
+        items[largest] = temp;
+        // Call heapRebuild on node in new location
+        heapRebuild(largest);
+    }
+}
 
 template <typename ItemType>
 void ArrayMaxHeap<ItemType>::heapCreate() {}
