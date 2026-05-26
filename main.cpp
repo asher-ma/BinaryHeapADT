28f7059945276bff763a5837e4be0f4969dcbec1
asher-ma <mangelas@uw.edu>
2026-05-26 17:08:14 +0000

    Implemented heapSort tests

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-#include "ArrayMaxHeap.h"
+#include "HeapSort.h"
 
 int main (){
     // Make array
@@ -27,5 +27,29 @@ int main (){
     std::cout << "\nClearing Heap...\n";
     heap.clear();
     std::cout << "isEmpty: " << (heap.isEmpty() ? "true" : "false") << "\n";
+
+    
+    // Testing Heapsort
+    // ---------------------------------------------------------------------------------------------
+    std::cout << "\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n" <<
+            "Testing Heapsort\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
+
+    // Make unsorted array
+    int array2[10] = {2, 5, 8, 1, 3, 6, 10, 4, 7, 9};
+    std::cout << "\nUnsorted Array: ";
+    for (int item : array2) {
+        std::cout << item << " ";
+    }
+
+    // callHeapsort on array
+    std::cout << "\nSorting...";
+    heapSort(array2, 10);
+
+    // Print sorted array
+    std::cout << "\nSorted Array: ";
+    for (int item : array2) {
+        std::cout << item << " ";
+    }
+    std::cout << "\n";
     return 0;
 }
