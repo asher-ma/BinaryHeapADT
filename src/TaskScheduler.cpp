65a1ae14dd7ea36401d2bdda34844fb37afe3480
asher-ma <asherhmangel@gmail.com>
2026-05-29 03:19:35 +0000

    Implements displayTasks

--- src/TaskScheduler.cpp
+++ src/TaskScheduler.cpp
@@ -26,15 +26,26 @@ void TaskScheduler<ItemType>::addTask(const std::string taskId, const int priori
 // Print task id to demonstrate function
 template <typename ItemType>
 void TaskScheduler<ItemType>::executeTask() {
-    std::cout << "Executing task: " << taskQueue.peek().id;
+    std::cout << "Executing task: " << taskQueue.peek().id << std::endl;
     taskQueue.remove();
 }
 
 // Print next tasks attributes
 template <typename ItemType>
 void TaskScheduler<ItemType>::peekNextTask() const {
-    std::cout << "Next task: " << taskQueue.peek();
+    std::cout << "Next task: " << taskQueue.peek() << std::endl;
 }
 
 template <typename ItemType>
-void TaskScheduler<ItemType>::displayTasks() const {}
+void TaskScheduler<ItemType>::displayTasks() const {
+    std::cout << "All tasks: " << std::endl;
+    HeapPriorityQueue<Task> dupeQueue(taskQueue);
+    while(!dupeQueue.isEmpty()) { // While queue is not empty
+        std::cout << dupeQueue.peek(); // Print task
+        heap.remove(); // Remove task
+        if (!dupeQueue.isEmpty()) { // If not empty
+            std::cout << "  |  "; // Print spacer
+        }
+    }
+    std::cout << std::endl;
+}
