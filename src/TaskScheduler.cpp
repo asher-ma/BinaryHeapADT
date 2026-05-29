312b0a92bf40ba3a9c0a54ee70087cce89cf47a4
asher-ma <asherhmangel@gmail.com>
2026-05-29 03:05:06 +0000

    Implements peekNextTask()

--- src/TaskScheduler.cpp
+++ src/TaskScheduler.cpp
@@ -13,6 +13,7 @@ TaskScheduler<ItemType>::TaskScheduler() {}
 // Public Methods
 // -----------------------------------------------------------------------------------------
 
+// Creates task object and adds it to taskQueue
 template <typename ItemType>
 void TaskScheduler<ItemType>::addTask(const std::string taskId, const int priority, const std::string taskDesc) {
     Task currentTask;
@@ -22,14 +23,18 @@ void TaskScheduler<ItemType>::addTask(const std::string taskId, const int priori
     return taskQueue.add(currentTask);
 }
 
+// Print task id to demonstrate function
 template <typename ItemType>
 void TaskScheduler<ItemType>::executeTask() {
     std::cout << "Executing task: " << taskQueue.peek().id;
     taskQueue.remove();
 }
 
+// Print next tasks attributes
 template <typename ItemType>
-void TaskScheduler<ItemType>::peekNextTask() const {}
+void TaskScheduler<ItemType>::peekNextTask() const {
+    std::cout << "Next task: " << taskQueue.peek();
+}
 
 template <typename ItemType>
 void TaskScheduler<ItemType>::displayTasks() const {}
